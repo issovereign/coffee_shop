35fdad741b55b4f885d6720325f82d2699055343
issovereign <fangzuo0214@gmail.com>
2023-10-15 01:02:59 +0800

    experiment: tasks app
    use tasks as a template for our later design

--- neo4j/neo4j/urls.py
+++ neo4j/neo4j/urls.py
@@ -24,6 +24,7 @@ import catalog.views as views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('tasks/', include('tasks.urls')),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/')),
     path('accounts/', include('django.contrib.auth.urls'))
