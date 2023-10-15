709453ad1b379528632e29e92391fd28fb0558b9
Peng_Jia_Wang <aaa7788h@gmail.com>
2023-10-15 22:12:45 +0800

    Add "order" models

--- CoffeeShop/Shopping/admin.py
+++ CoffeeShop/Shopping/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import member
+from .models import member, order
 
 # Register your models here.
 class memberinfo(admin.ModelAdmin):
     list_display = ('member_name', 'member_number', 'member_address', 'member_email')
 
+class ordersinfo(admin.ModelAdmin):
+    list_display = ('primary_key', 'member_name', 'coffee_name')
+
 admin.site.register(member, memberinfo)
+admin.site.register(order, ordersinfo)
--- CoffeeShop/Shopping/models.py
+++ CoffeeShop/Shopping/models.py
@@ -17,3 +17,15 @@ class member(models.Model):
 
     def get_absolute_url(self):
         return reverse("member_detail", args=[str(self.id)])
+
+class order(models.Model):
+    primary_key = models.CharField(max_length=200, help_text='Enter member primary key.')
+    member_name = models.CharField(max_length=200, help_text='Enter member name.')
+    coffee_name = models.CharField(max_length=200, help_text='Enter coffee name')
+
+    def __str__(self):
+        return self.member_name
+    
+    class Meta:
+        ordering = ['member_name']
+    
